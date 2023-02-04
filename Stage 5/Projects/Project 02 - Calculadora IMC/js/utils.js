48ddd1931e1993b924802dc91626e9f1ab44c725
Carlos Alberto <devcarlosalberto@gmail.com>
2023-02-04 20:07:42 -0300

    Delete Stage 5 directory

--- Stage 5/Projects/Project 02 - Calculadora IMC/js/utils.js
+++
@@ -1,7 +0,0 @@
-export function calculateIMC(weight, height) {
-    return (weight / (height / 100) ** 2).toFixed(2)
-}
-
-export function notANumber(value) {
-    return isNaN(value) || value == ""
-}
